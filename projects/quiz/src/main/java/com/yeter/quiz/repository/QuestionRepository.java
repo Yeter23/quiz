1fbe2c005728405e33a2d8b8e0e5bb0d5057e548
Yeter23 <yeteryusifova00@gmail.com>
2024-11-28 01:51:35 +0400

    Question repo yaradildi

---
+++ projects/quiz/src/main/java/com/yeter/quiz/repository/QuestionRepository.java
@@ -0,0 +1,8 @@
+package com.yeter.quiz.repository;
+
+import com.yeter.quiz.model.Questions;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuestionRepository  extends JpaRepository<Questions,Long> {
+    
+}
